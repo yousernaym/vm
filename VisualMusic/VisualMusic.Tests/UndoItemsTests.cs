6c321094287d7bf4fabc037848e32d70b09919cc
yousernaym <mickeh.se@hotmail.com>
2026-07-23 08:57:09 +0200

    Increase undo restore coverage

--- VisualMusic/VisualMusic.Tests/UndoItemsTests.cs
+++ VisualMusic/VisualMusic.Tests/UndoItemsTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Midi;
+using VisualMusic.Keyframes;
 using Xunit;
 
 namespace VisualMusic.Tests
@@ -92,22 +93,56 @@ namespace VisualMusic.Tests
             try
             {
                 var live = ProjectWithAudioTrack("a.wav");
+                var tp = live.TrackViews[0].TrackProps;
+
                 live.Props.FadeIn = 1.5f;
-                live.TrackViews[0].TrackProps.MaterialProps.Transp = 0.25f;
+                tp.StyleProps.Type = NoteStyleType.Bar;
+                tp.StyleProps.GetLineStyle().LineWidth = 2.5f;
+                tp.MaterialProps.Transp = 0.25f;
+                tp.MaterialProps.Hue = 0.4f;
+                tp.LightProps.AmbientAmount = 0.3f;
+                tp.SpatialProps.XOffset = 1.25f;
+                live.PropertyKeyframes.Add("proj/BackgroundImageOpacity", 0, KfInterpolation.Linear,
+                    new ScalarKfValue(0.2));
+                live.PropertyKeyframes.Add("track/0/Transp", 100, KfInterpolation.Smooth,
+                    new ScalarKfValue(0.5));
 
                 var undo = new UndoItems();
                 undo.Add("before edit", live);
 
                 live.Props.FadeIn = 3f;
-                live.TrackViews[0].TrackProps.MaterialProps.Transp = 0.9f;
+                tp.StyleProps.Type = NoteStyleType.Line;
+                tp.StyleProps.GetLineStyle().LineWidth = 9f;
+                tp.MaterialProps.Transp = 0.9f;
+                tp.MaterialProps.Hue = 0.8f;
+                tp.LightProps.AmbientAmount = 0.9f;
+                tp.SpatialProps.XOffset = -4f;
+                tp.AudioProps.Filename = "b.wav";
+                live.PropertyKeyframes = new KeyframeSet();
+                live.PropertyKeyframes.Add("proj/BackgroundImageOpacity", 50, KfInterpolation.Hold,
+                    new ScalarKfValue(1));
                 undo.Add("after edit", live);
 
                 undo--;
                 live.CopyPropsFrom(undo.Current.Project);
+                tp = live.TrackViews[0].TrackProps;
 
                 Assert.Equal(1.5f, live.Props.FadeIn);
-                Assert.Equal(0.25f, live.TrackViews[0].TrackProps.MaterialProps.Transp!.Value);
-                Assert.Equal("a.wav", live.TrackViews[0].TrackProps.AudioProps.Filename);
+                Assert.Equal(NoteStyleType.Bar, tp.StyleProps.Type);
+                Assert.Equal(2.5f, tp.StyleProps.GetLineStyle().LineWidth!.Value);
+                Assert.Equal(0.25f, tp.MaterialProps.Transp!.Value);
+                Assert.Equal(0.4f, tp.MaterialProps.Hue!.Value);
+                Assert.Equal(0.3f, tp.LightProps.AmbientAmount!.Value);
+                Assert.Equal(1.25f, tp.SpatialProps.XOffset!.Value);
+                Assert.Equal("a.wav", tp.AudioProps.Filename);
+                Assert.True(live.PropertyKeyframes.HasKeyAt("proj/BackgroundImageOpacity", 0));
+                Assert.True(live.PropertyKeyframes.HasKeyAt("track/0/Transp", 100));
+                Assert.False(live.PropertyKeyframes.HasKeyAt("proj/BackgroundImageOpacity", 50));
+                Assert.Equal(KfInterpolation.Linear,
+                    live.PropertyKeyframes.GetInterpolation("proj/BackgroundImageOpacity", 0));
+                Assert.Equal(0.2,
+                    ((ScalarKfValue)live.PropertyKeyframes.Tracks["proj/BackgroundImageOpacity"]
+                        .FindBrackets(0).Before.Value).V, 5);
             }
             finally
             {
